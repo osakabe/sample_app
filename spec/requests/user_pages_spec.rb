d3ab23690c24199d6007dabbbe1e851c8123e911
haru <osakabekamimachi@gmail.com>
2012-12-05 17:21:00 +0900

    commit

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
-describe "UserPages" do
+describe "User pages" do
   subject{ page }
-  describe "signup page" do
-    before{ visit signup_path }
-    it { should have_selector('h1', text: 'Sign up')}
-    it { should have_selector('title', text: full_title('Sign up'))} 
+  
+  describe "sign page" do
+    before { visit signup_path }
+    
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name)}
   end
 end
